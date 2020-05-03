a1d38b507bcf9e7b2b7e2e9fe74b6d2898b68d9e
gatosyocora <kut190316q@gmail.com>
2020-05-03 22:18:31 +0900

    指定されたパスで存在しないフォルダを作成する関数を作成

--- Assets/NoBlinkSetter/Editor/GatoEditorUtility.cs
+++ Assets/NoBlinkSetter/Editor/GatoEditorUtility.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System;
+using System.IO;
 
 public class GatoEditorUtility
 {
+    private const char BSLASH = '\\';
+
     public static void NonIndentHelpBox(string message, MessageType messageType)
     {
         var currentIndentLevel = EditorGUI.indentLevel;
@@ -23,5 +26,39 @@ public class GatoEditorUtility
             action.Invoke();
         }
         EditorGUI.indentLevel = currentIndentLevel;
+    }
+
+    public static bool CreateNoExistFolders(string path)
+    {
+        string directoryPath;
+        if (string.IsNullOrEmpty(Path.GetExtension(path)))
+        {
+            directoryPath = path;
+        }
+        else
+        {
+            directoryPath = Path.GetDirectoryName(path);
+        }
+
+        if (!Directory.Exists(directoryPath))
+        {
+            var directories = directoryPath.Split(BSLASH);
+
+            directoryPath = "Assets";
+            for (int i = 1; i < directories.Length; i++)
+            {
+                if (!Directory.Exists(directoryPath +BSLASH+ directories[i]))
+                {
+                    AssetDatabase.CreateFolder(directoryPath, directories[i]);
+                }
+
+                directoryPath += BSLASH + directories[i];
+            }
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            return true;
+        }
+
+        return false;
     }
 }
