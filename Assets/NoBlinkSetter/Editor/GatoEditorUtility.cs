b552bbbc8f89952df075167325918c5767c7e94a
gatosyocora <kut190316q@gmail.com>
2020-05-03 21:04:32 +0900

    Editorを設計する際の便利クラスを作成

---
+++ Assets/NoBlinkSetter/Editor/GatoEditorUtility.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class GatoEditorUtility
+{
+    public static void NonIndentHelpBox(string message, MessageType messageType)
+    {
+        var currentIndentLevel = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+        EditorGUILayout.HelpBox(message, messageType);
+        EditorGUI.indentLevel = currentIndentLevel;
+    }
+
+    public static void NonIndentButton(string text, Action action)
+    {
+        var currentIndentLevel = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+        if (GUILayout.Button(text))
+        {
+            action.Invoke();
+        }
+        EditorGUI.indentLevel = currentIndentLevel;
+    }
+}
